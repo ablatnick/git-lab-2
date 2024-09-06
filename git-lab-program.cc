4909c914c338d581e43f9035ef3aeea8266d7325
ablatnick <168376829+ablatnick@users.noreply.github.com>
2024-09-06 16:08:09 -0400

    Add files via upload

---
+++ git-lab-program.cc
@@ -0,0 +1,17 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Alec Blatnick
+ *        Date: 09/6/24
+ * Description: git-hub-lab
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+
+ cout << "Hello Git!!" << endl;
+    return 0;
+}// main
